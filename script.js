e0b25935ff0cd632e3439a5a3511eaa19e49bef6
projectai09 <vatsarun58@gmail.com>
2024-11-22 17:18:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select the button with the class "btn-17"
+const signInButton = document.querySelector(".btn-17");
+
+// Add a click event listener to the button
+signInButton.addEventListener("click", () => {
+  // Redirect to the desired page
+  window.location.href = "login.html"; // Replace with your target URL
+});
